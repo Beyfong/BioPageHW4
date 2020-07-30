8468f36205a744c45248f78fa56e9a3effa9d927
AMBAL-DAMBAL <ablai2602@gmail.com>
2020-07-30 16:32:24 +0600

    Initial Commit

---
+++ Oprosnik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oprosnik
+//
+//  Created by ablai erzhanov on 7/30/20.
+//  Copyright © 2020 ablai erzhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
